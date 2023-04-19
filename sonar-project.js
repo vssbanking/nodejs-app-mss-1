2d724d25cdace382975f9226db82968ee7cec915
vineethreddydesireddy <127300845+vineethreddydesireddy@users.noreply.github.com>
2023-04-19 17:36:26 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.126.192.197:9000/',
+  serverUrl: 'http://172.31.47.237:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
