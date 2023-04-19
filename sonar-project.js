6ba29c24c310685a6d4ecaf35cd069789375c913
vineethreddydesireddy <127300845+vineethreddydesireddy@users.noreply.github.com>
2023-04-19 17:23:05 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.38.96.100:9000/',
+  serverUrl: 'http://13.126.192.197:9000/projects/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_0843edd7e8aa8bb8f2cd0b2fa624a1f76e5fdc96',
+	    'sonar.login': 'squ_99eeed4fb520f78eaf4c69779700cbfd965291fa',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
